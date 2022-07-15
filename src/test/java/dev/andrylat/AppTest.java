faf05caa6f940a013197c5a01f8109dfdcba0655
Andrii Latyshev <andrii.latyshev@gmail.com>
2022-07-15 13:28:24 +0300

    Added tests + CI

--- src/test/java/dev/andrylat/AppTest.java
+++ src/test/java/dev/andrylat/AppTest.java
@@ -1,8 +1,8 @@
 package dev.andrylat;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple App.
@@ -11,8 +11,20 @@ public class AppTest {
     /**
      * Rigorous Test :-)
      */
+    App app = new App();
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    void test_both_are_positive() {
+        int actual = app.sum(1,2);
+        int expected = 3;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void test_both_are_negative() {
+        int actual = app.sum(-1,-2);
+        int expected = -3;
+
+        assertEquals(expected, actual);
     }
 }
